5d708b8780aee3ea72a54521b4867046cb4f2ed9
孤月蓝风 <trlanfeng@foxmail.com>
2016-03-28 01:53:54 +0800

    初始化项目

---
+++ Assets/TRRunner/Runner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace TRRunner
+{
+    /// <summary>
+    /// 角色类
+    /// </summary>
+    public class Runner : MonoBehaviour
+    {
+        /// <summary>
+        /// 允许跳跃的次数
+        /// </summary>
+        public int JumpsTimeAllowed { get; set; }
+        /// <summary>
+        /// 跳跃的力的大小
+        /// </summary>
+        public float  JumpForce { get; set; }
+        /// <summary>
+        /// 两次跳跃之间的冷却时间
+        /// </summary>
+        public float CooldownBetweenJumps { get; set; }
+        /// <summary>
+        /// 是否仅可以在地面时跳跃
+        /// </summary>
+        public bool JustCanJumpOnGround { get; set; }
+    }
+
+}
