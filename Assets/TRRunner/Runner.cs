d47bd0ece55bb3d94f76d713d921625269f8eae2
孤月蓝风 <trlanfeng@foxmail.com>
2016-03-28 23:42:20 +0800

    1.添加基本素材 2.添加 Ground 标签 3.开发角色跳跃控制脚本

--- Assets/TRRunner/Runner.cs
+++ Assets/TRRunner/Runner.cs
@@ -10,19 +10,74 @@ namespace TRRunner
         /// <summary>
         /// 允许跳跃的次数
         /// </summary>
-        public int JumpsTimeAllowed { get; set; }
+        public int JumpsTimeAllowed = 1;
         /// <summary>
         /// 跳跃的力的大小
         /// </summary>
-        public float  JumpForce { get; set; }
+        public float JumpForce = 1;
         /// <summary>
         /// 两次跳跃之间的冷却时间
         /// </summary>
-        public float CooldownBetweenJumps { get; set; }
+        public float CooldownBetweenJumps = 0.5f;
         /// <summary>
         /// 是否仅可以在地面时跳跃
         /// </summary>
-        public bool JustCanJumpOnGround { get; set; }
+        public bool JustCanJumpOnGround = true;
+
+        public bool isOnGround = false;
+        public bool isCoolDown = false;
+        public float coolDownTimer = 0;
+
+        private Rigidbody2D R2D;
+
+        void Start()
+        {
+            R2D = this.transform.GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            checkCoolDown();
+            Jump();
+        }
+
+        void Jump()
+        {
+            if (Input.GetAxis("Fire1") != 0 && isOnGround && isCoolDown)
+            {
+                R2D.AddForce(Vector2.up * JumpForce);
+                coolDownTimer = 0;
+            }
+        }
+
+        void checkCoolDown()
+        {
+            if (coolDownTimer <= CooldownBetweenJumps && coolDownTimer >= 0)
+            {
+                coolDownTimer += Time.deltaTime;
+                isCoolDown = false;
+            }
+            else
+            {
+                isCoolDown = true;
+            }
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                isOnGround = true;
+            }
+        }
+
+        void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                isOnGround = false;
+            }
+        }
     }
 
 }
