7e6c4f245a63f70a0edd03026d5d301cc280d23c
孤月蓝风 <trlanfeng@foxmail.com>
2016-05-02 15:38:10 +0800

    1.增加关卡配置 2.分离导航器和管理器

---
+++ Assets/TRRunner/Level/LevelScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public class LevelScript : MonoBehaviour
+{
+    // 关卡名称
+    public string levelName;
+    // 关卡ID
+    public int levelID;
+    // 过关所需点数
+    public int needPoint;
+    // 默认初始速度
+    public int defaultSpeed;
+    // 速度刷新周期
+    public int speedPlusTimer;
+    // 速度增加量
+    public int speedPlusNumber;
+}
